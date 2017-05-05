cf7eb2aaf55d3626ae5eeef27346ce9829748ef8
Habib Ullah <h853@outlook.com>
2017-05-05 19:42:34 +0500

    start of contact us form with php

---
+++ php/contactUs.php
@@ -0,0 +1,11 @@
+<?php
+if (isset($_POST["submit"])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $number = $_POST['number'];
+    $message = $_POST['message'];
+    $subject = $_POST['subject'];
+    $to = 'sherazahmed93@gmail.com';
+}
+?>
+
